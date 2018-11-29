085c881e27a66ae5c76176411c5a66da9241bc10
Aleksandar Radosevic <acafacafunnyface@gmail.com>
2018-11-29 19:19:23 +0100

    Project initialization

---
+++ app/src/main/java/archon/solutions/com/backgroundstopwatch/ui/activity/RouteActivity.java
@@ -0,0 +1,9 @@
+package archon.solutions.com.backgroundstopwatch.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import archon.solutions.com.backgroundstopwatch.R;
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_route)
+public class RouteActivity extends AppCompatActivity {
+}
